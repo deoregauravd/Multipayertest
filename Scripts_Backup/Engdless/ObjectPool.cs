9df5f250f37a6215c1981b26922b78f788bea463
Gaurav Deore <102516723+deoregauravd@users.noreply.github.com>
2023-01-15 16:07:38 +0530

    - Added Pause Menu

--- Scripts_Backup/Engdless/ObjectPool.cs
+++ Scripts_Backup/Engdless/ObjectPool.cs
@@ -38,7 +38,6 @@ public class ObjectPool : MonoBehaviour
             }
            
         }
-
         
         return null;
     }
--- Scripts_Backup/Engdless/PlayerMovement.cs
+++ Scripts_Backup/Engdless/PlayerMovement.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{  
+    // Player Physics
     public Transform playerMovement;
     public CharacterController player;
     public float speed = 1f;
     public float moveSpeed;
     public  Vector3 movement;
+   
+
+    // Player GUI
+    public static int health;
+    public int startHealth = 3;
+    public float distanceCover;
+   
+
+
+    // Player Animation
     private Animator anim;
     public  float jumpspeed = 1f;
     public Transform scenetwoPos;
+
+    // Scenes in Game
     public GameObject sceneOne;
     public GameObject sceneTwo;
     public GameObject sceneThree;
@@ -36,11 +49,14 @@ public class PlayerMovement : MonoBehaviour
     {
         player = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+
+        health = startHealth;
     }
 
     private void Update()
     {
         UpdateAnimatorAnimation();
+        DistanceCovered();
     }
 
     private void UpdateAnimatorAnimation()
@@ -57,8 +73,7 @@ public class PlayerMovement : MonoBehaviour
         else if (player.velocity.z > 1)
         {
             movement.y -= 6 * Time.deltaTime;
-            
-           // Debug.Log("Centerchage");
+            //Debug.Log("Centerchage");
             state = MovementState.run;
         }
         else
@@ -71,7 +86,7 @@ public class PlayerMovement : MonoBehaviour
             state = MovementState.right;
            // Debug.Log("Right");
         }
-        else if (player.velocity.x < 0)//(-0.1f))
+        else if (player.velocity.x < 0) //(-0.1f))
         {
             state = MovementState.left;
             //Debug.Log("left");
@@ -86,26 +101,16 @@ public class PlayerMovement : MonoBehaviour
         player.Move(movement);
 
         player.center = new Vector3(player.center.x, 8f, player.center.z);
-       UpdateRoad();
-
-    }
-
-    void FixedUpdate()
-    {
       
+        UpdateRoad();
 
-    }
-
-    private void LateUpdate()
-    {
-      //  player.center = new Vector3(player.center.x, 56.1f, player.center.z);
     }
 
     public void UpdateRoad()
     {
         if (playerMovement.position.z > 15 && playerMovement.position.z < 55 )
         {
-            //GameObject scenetwo  = GameObject.FindGameObjectWithTag("sceneTwo");
+            //GameObject scenetwo = GameObject.FindGameObjectWithTag("sceneTwo");
             sceneTwo.transform.position = new Vector3(0, 0, 55);
             sceneTwo.SetActive(true);
             Debug.Log("scene TwoActivation");
@@ -126,63 +131,38 @@ public class PlayerMovement : MonoBehaviour
             sceneOne.SetActive(true);
             sceneTwo.SetActive(false);
             Debug.Log("scene one Reactivation");
-
         }
 
         void HurdleGenration()
         {
 
-
-            /* foreach (GameObject hurdle in  ObjectPool.SharedInstance.pooledObjects)
-             {
-                // hurdle.transform.position = new Vector3(0, 0, 55);
-                 //hurdle.transform.position = new Vector3(0, 0, 110);
-                 hurdle.gameObject.SetActive(true);
-             }
-
- */
             GameObject hurdle = ObjectPool.SharedInstance.GetPooledObject();
             for (int i = 0; i < ObjectPool.SharedInstance.pooledObjects.Count; i++)
             {
                 if (hurdle != null) // if you dont use this codition its going to return null value from object pooling and it will cause null reference
                 {
                     Vector3 position = hurdlePos[i].position;
-                    ObjectPool.SharedInstance.pooledObjects[i].transform.position = position;//new Vector3(0, 5, (i * 30));
+                    ObjectPool.SharedInstance.pooledObjects[i].transform.position = position; //new Vector3(0, 5, (i * 30));
                     hurdle.gameObject.SetActive(true);
                 }
                 
             }
-            /*  if (hurdle != null)
-              {
-                  hurdle.transform.position = new Vector3(0, 0, 55);
-                  hurdle.transform.position = new Vector3(0, 0, 110);
-                  hurdle.gameObject.SetActive(true);
-              }*/
+          
         }
-
-
-
-
-
        
     }
-
-
-    /*public void UpdateRoad()
+    public void DistanceCovered()
     {
-       if (playerMovement.position.z > 10)
-        {
-            Debug.Log("moment of z is dectected");
-            ObjectPool.SharedInstance.GetPooledObject();
-            transform.position = new Vector3(0, 0, 55);
-            ObjectPool.SharedInstance.GetPooledObject().SetActive(true);
-            
-        }
-    }*/
+        distanceCover = playerMovement.position.z;
+
+      //  Debug.Log(distanceCover + "distacecover");
+    }
 }
 
 
 // code used priviously 
+
+
 /*  if (player.isGrounded)
       {
           movement.y = 0;
@@ -206,4 +186,36 @@ public class PlayerMovement : MonoBehaviour
     void DefaultChan()
     {
         player.center = new Vector3(player.center.x, 55f, player.center.z);
-    }*/
+    }
+
+
+  /*  if (hurdle != null)
+              {
+                  hurdle.transform.position = new Vector3(0, 0, 55);
+                  hurdle.transform.position = new Vector3(0, 0, 110);
+                  hurdle.gameObject.SetActive(true);
+              }*/
+
+
+
+/*public void UpdateRoad()
+{
+   if (playerMovement.position.z > 10)
+    {
+        Debug.Log("moment of z is dectected");
+        ObjectPool.SharedInstance.GetPooledObject();
+        transform.position = new Vector3(0, 0, 55);
+        ObjectPool.SharedInstance.GetPooledObject().SetActive(true);
+
+    }
+}*/
+
+
+/* foreach (GameObject hurdle in  ObjectPool.SharedInstance.pooledObjects)
+ {
+    // hurdle.transform.position = new Vector3(0, 0, 55);
+     //hurdle.transform.position = new Vector3(0, 0, 110);
+     hurdle.gameObject.SetActive(true);
+ }
+
+*/
